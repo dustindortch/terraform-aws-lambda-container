f5be54f3bef9ff7effb657c6efad10722d2f801a
Dustin Dortch <me@dustindortch.com>
2024-06-20 12:46:35 -0400

    New test

--- main.tf
+++ main.tf
@@ -1,25 +1,111 @@
-locals {
-  original_map = {
-    key1 = {
-      attr1 = "value1"
-      attr2 = "value2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-    key2 = {
-      attr1 = "value3"
-      attr2 = "value4"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
     }
   }
-  update_map = {
-    for k, v in local.original_map : k => merge(
-      v,
-      {
-        attr3 = "value5"
-      }
-    ) if v.attr1 == "value1"
-  }
-  combined_map = merge(local.original_map, local.update_map)
 }
 
-output "output_map" {
-  value = local.combined_map
+provider "aws" {}
+
+provider "github" {}
+
+data "github_repository" "repo" {
+  name = var.github_repository_contents.name
+}
+
+data "github_branch" "branch" {
+  repository = data.github_repository.repo.name
+  branch     = var.github_repository_contents.branch == null ? data.github_repository.repo.default_branch : var.github_repository_contents.branch
+}
+
+data "github_tree" "tree" {
+  recursive  = true
+  repository = data.github_repository.repo.name
+  tree_sha   = data.github_branch.branch.sha
+}
+
+locals {
+  tree_files = { for i in data.github_tree.tree.entries : i.sha => i }
+}
+
+data "github_repository_file" "file" {
+  for_each = local.tree_files
+
+  repository = data.github_repository.repo.name
+  branch     = data.github_branch.branch.ref
+  file       = each.value.path
+}
+
+locals {
+  file_contents = {
+    for k, v in data.github_repository_file.file : k => {
+      content = v.content
+      path    = local.tree_files[k].path
+    } if local.tree_files[k].type == "blob" && !startswith(local.tree_files[k].path, ".")
+  }
+}
+
+resource "local_file" "files" {
+  for_each = local.file_contents
+
+  filename = "${path.cwd}/${each.value.path}"
+  content  = each.value.content
+}
+
+resource "aws_ecr_repository" "lambda" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_ecr_authorization_token" "lambda" {}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+
+  registry_auth {
+    address  = data.aws_ecr_authorization_token.lambda.proxy_endpoint
+    username = data.aws_ecr_authorization_token.lambda.user_name
+    password = data.aws_ecr_authorization_token.lambda.password
+  }
+}
+
+locals {
+  build_stage = merge(
+    var.build_stage,
+    {
+      path = var.path
+    }
+  )
+}
+
+module "docker" {
+  depends_on = [local_file.files]
+  source     = "app.terraform.io/DustinDortch/aws-lambda/docker"
+  version    = "~> 1.0"
+
+  name    = aws_ecr_repository.lambda.repository_url
+  handler = var.handler
+  path    = var.path
+
+  base_image = var.base_image
+
+  build_stage = local.build_stage
+}
+
+output "registry_image_name" {
+  value = module.docker.registry_image_name
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,45 @@
-# variables.tf
+variable "base_image" {
+  description = "The base image to use for the container image."
+  type = object({
+    name     = string
+    registry = optional(string, null)
+    tag      = optional(string, null)
+  })
+}
 
-## Guidelines
+variable "build_stage" {
+  default     = null
+  description = "The build stage to use for the container image."
+  type = object({
+    name     = optional(string, null)
+    registry = optional(string, null)
+    tag      = optional(string, null)
+  })
+}
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository (container namespace and name) to publish container image."
+  type        = string
+}
 
-## Create variables for as many values as possible. This will make it easier to update values in the future.
+variable "ecr_registry_id" {
+  description = "The registry ID (AWS Account) of the ECR repository to publish container image."
+  type        = string
+}
 
-## Use intuitive names for variables. This will make it easier for others to understand your code.
+variable "github_repository_contents" {
+  description = "The GitHub repository release to use for the application code."
+  type = object({
+    name   = string
+    branch = optional(string, null)
+  })
+}
 
-## The principal resource of the module should have a variable called 'name' associated with it, not 'resource_name'.
+variable "handler" {
+  description = "The AWS Lambda handler to invoke within the container image."
+  type        = string
+}
 
-## Only one prinicpal instance of a principal resource should be created within its module.  If multiple instances are required, the calling module can implement it through a `count` or `for_each` meta-argument.
-
-## Use input validation on variables.  It limits the logic and code required within the rest of the configuration.
-
-## Use types.
+variable "path" {
+  description = "The path in the GitHub repository to the application code to copy."
+  type        = string
+}
